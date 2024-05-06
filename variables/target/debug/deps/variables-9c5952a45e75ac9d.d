a8f573b077cb63753f344fb0be418b5f82d8a723
Sathwik <146921254+chirivelli@users.noreply.github.com>
2024-05-06 08:30:44 +0000

    Lesson 3.1

---
+++ variables/target/debug/deps/variables-9c5952a45e75ac9d.d
@@ -0,0 +1,5 @@
+/workspaces/ferris/variables/target/debug/deps/variables-9c5952a45e75ac9d: src/main.rs
+
+/workspaces/ferris/variables/target/debug/deps/variables-9c5952a45e75ac9d.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/workspaces/ferris/variables/target/debug/variables: /workspaces/ferris/variables/src/main.rs
