5aade208f8f3960a47dd0eff191717e82268e3d0
Sathwik <146921254+chirivelli@users.noreply.github.com>
2024-05-07 10:09:51 +0000

    Lesson 3.5

---
+++ loops/target/debug/deps/loops-2ce153b663589422.d
@@ -0,0 +1,5 @@
+/workspaces/ferris/loops/target/debug/deps/loops-2ce153b663589422: src/main.rs
+
+/workspaces/ferris/loops/target/debug/deps/loops-2ce153b663589422.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/workspaces/ferris/loops/target/debug/loops: /workspaces/ferris/loops/src/main.rs
